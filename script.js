b361b768b59b4ec950aee74f8c8a803d99e41844
duyenluu <duyen.luuhai@gmail.com>
2019-05-09 19:53:01 -0400

    link email to gsheet

--- script.js
+++ script.js
@@ -43,19 +43,71 @@ function showSlidesAutmatic() {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 
-  setTimeout(showSlidesAutmatic, 6000); // Change image every 2 seconds
+  setTimeout(showSlidesAutmatic, 7000); // Change image every 2 seconds
 }
 
-// Take value from input field
-
+// Clear value field when clicked
 var input_field = document.getElementById("input-field");
 input_field.addEventListener("click", clearValue);
 function clearValue(){
   document.getElementById("input-field").value="";
 }
 
-// var submit_btn = document.getElementById("submit_btn");
-// submit_btn.addEventListener("click", storeValue);
-// function storeValue(){
+// Take input from field to Google Sheets
+var $input = $('input#input-field'),
+    url = 'https://script.google.com/macros/s/AKfycbyH6HKeDNw_J7R-uGTcdQLUzeYiptWg23wwFB-5In7g3oX1UtA/exec'
 
-// }
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+// $('#submit-btn').on('click', function(e) {
+//   e.preventDefault();
+//   var jqxhr = $.ajax({
+//     url: url,
+//     method: "GET",
+//     dataType: "json",
+//     data: $input.serializeObject(),
+//     success: function(e){console.log(e);}
+//   });
+// });
+
+// Validate if input is an email
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  // var $result = $("#result");
+  var email = $("#input-field").val();
+  if (validateEmail(email)) {
+    var jqxhr = $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json",
+      data: $input.serializeObject(),
+      success: function(e){console.log(e);}
+    });
+    alert("Thank you! Your email has been submitted.");
+
+  } else {
+    alert("Please enter an email.");
+  }
+  return false;
+}
+
+$("#submit-btn").on('click', validate);
+
